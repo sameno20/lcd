bc9534b584f18da2a22e5c16976d6010f2fcdc59
Sam Boyen <sam.boyen@student.ap.be>
2026-04-27 12:37:35 +0200

    done

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@ Sam Boyen s16729
 */
 
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 int Vooruit_knop = 23;
 int Achteruit_knop = 34; 
 int Links_knop = 35;
 int Rechts_knop = 32;
-
 int M1_IA = 18;
 int M1_IB = 5;
 int M2_IA = 2; 
-int M2_IB = 15; 
+int M2_IB = 15;
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 int Speed_pot = 39;
 
@@ -31,11 +33,16 @@ void setup(){
 	pinMode(M1_IB,OUTPUT);
   pinMode(M2_IA,OUTPUT);
   pinMode(M2_IB,OUTPUT);
+
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop(){
   while(digitalRead(Vooruit_knop)){
     Serial.println("Vooruit");
+    lcd.setCursor(0,0);
+    lcd.print("vooruit");
     Speed_read = map(analogRead(Speed_pot), 0, 4095, 0, 255);
     analogWrite(M1_IA, Speed_read);
     analogWrite(M1_IB, 0);
@@ -45,6 +52,8 @@ void loop(){
   }
   while(digitalRead(Achteruit_knop)){
     Serial.println("Achteruit");
+    lcd.setCursor(0,0);
+    lcd.print("achteruit");
     Speed_read = map(analogRead(Speed_pot), 0, 4095, 0, 255);
     analogWrite(M1_IB, Speed_read);
     analogWrite(M1_IA, 0);
@@ -54,6 +63,8 @@ void loop(){
   }
   while(digitalRead(Links_knop)){
     Serial.println("Links");
+    lcd.setCursor(0,0);
+    lcd.print("links");
     Speed_read = map(analogRead(Speed_pot), 0, 4095, 0, 255);
     analogWrite(M1_IA, Speed_read);
     analogWrite(M1_IB, 0);
@@ -63,6 +74,8 @@ void loop(){
   }
   while(digitalRead(Rechts_knop)){
     Serial.println("Rechts");
+    lcd.setCursor(0,0);
+    lcd.print("rechts");
     Speed_read = map(analogRead(Speed_pot), 0, 4095, 0, 255);
     analogWrite(M1_IB, Speed_read);
     analogWrite(M1_IA, 0);
@@ -74,6 +87,7 @@ void loop(){
   analogWrite(M1_IB, 0);
   analogWrite(M2_IA, 0);
   analogWrite(M2_IB, 0);
+  lcd.clear();
   delay(100);
 }
   
